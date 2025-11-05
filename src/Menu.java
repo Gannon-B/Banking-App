469994b234bf5cd7b521eda3b669bd3627a82b17
Gannon-B <gannon.a.becker@gmail.com>
2025-11-05 14:33:43 -0500

    Unified Scanner

--- src/Menu.java
+++ src/Menu.java
@@ -4,13 +4,15 @@ import java.util.ArrayList;
 
 public class Menu {
 
+    static Scanner scanner = new Scanner(System.in);
+
     public static void mainMenu() {
         System.out.println("\n\n\n\n\n\n\n");
         System.out.println("1. Choose Account");
         System.out.println("2. Create Account");
-        Scanner mainChoice = new Scanner(System.in);
         System.out.print("Enter your choice: ");
-        int choice = mainChoice.nextInt();
+        int choice = scanner.nextInt();
+        scanner.nextLine();
         if (choice == 1) {
             System.out.println("\n\n\n\n\n\n\n");
             chooseAccount();
@@ -24,12 +26,12 @@ public class Menu {
 
 
     public static void chooseAccount() {
-        Scanner accountChoice = new Scanner(System.in);
         for (int i = 0; i < Account.globalAccountList.size(); i++) {
             Account account = Account.globalAccountList.get(i);
             System.out.println("Account " + i + ": " + account.getAccountType());
         }
-        int choice = accountChoice.nextInt();
+        int choice = scanner.nextInt();
+        scanner.nextLine();
         System.out.println("\n\n\n\n\n\n\n");
         for (int i = 0; i < Account.globalAccountList.size(); i++) {
             if (choice == i) {
@@ -42,9 +44,8 @@ public class Menu {
 
 
     public static void createAccount() {
-        Scanner accountScanner = new Scanner(System.in);
         System.out.println("Enter Account Type:");
-        String accountType = accountScanner.nextLine();
+        String accountType = scanner.nextLine();
         double balance = 0;
         Account newAccount  = new Account(accountType, balance);
         Account.globalAccountList.add(newAccount);
@@ -52,7 +53,6 @@ public class Menu {
     }
 
     public static void displayAccount(Account account) {
-        Scanner accountScanner = new Scanner(System.in);
         account.getAccountType();
         account.getAccountNumber();
         account.getBalance();
@@ -64,22 +64,24 @@ public class Menu {
 
 
     public static void userPrompt(Account account) {
-        Scanner scanner = new Scanner(System.in);
         System.out.println("1. Deposit");
         System.out.println("2. Withdraw");
         System.out.print("Select Option: ");
         int choice = scanner.nextInt();
+        scanner.nextLine();
         System.out.print("\n\n\n\n\n\n\n");
 
         if (choice == 1){
             System.out.println("Enter Deposit Amount:");
             double amountDeposited = scanner.nextInt();
+            scanner.nextLine();
             account.Deposit(amountDeposited);
             Menu.mainMenu();
         }
         else if (choice == 2){
             System.out.println("Enter Withdraw Amount:");
             double amountWithdrawn = scanner.nextInt();
+            scanner.nextLine();
             account.Withdraw(amountWithdrawn);
             Menu.mainMenu();
         }
