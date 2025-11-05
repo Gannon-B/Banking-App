3a6f8db0ef5411c96d96f3f24a3b06f78daf1a6f
Gannon-B <gannon.a.becker@gmail.com>
2025-11-05 15:45:37 -0500

    Back buttons

--- src/Menu.java
+++ src/Menu.java
@@ -24,33 +24,54 @@ public class Menu {
     }
 
 
-
     public static void chooseAccount() {
         for (int i = 0; i < Account.globalAccountList.size(); i++) {
             Account account = Account.globalAccountList.get(i);
             System.out.println("Account " + i + ": " + account.getAccountType());
         }
-        int choice = scanner.nextInt();
-        scanner.nextLine();
+        System.out.println("Enter your account Index or enter 'back' to return to menu: ");
+        String input = scanner.nextLine();
         System.out.println("\n\n\n\n\n\n\n");
-        for (int i = 0; i < Account.globalAccountList.size(); i++) {
-            if (choice == i) {
-                displayAccount(Account.globalAccountList.get(i));
+        if (input.equalsIgnoreCase("back")) {
+            mainMenu();
+        }
+        else if (!input.equalsIgnoreCase("back")) {
+            try {
+                int choice = Integer.parseInt(input);
+                if (choice > Account.globalAccountList.size() - 1 || choice < 0) {
+                    System.out.println("Invalid choice");
+                    chooseAccount();
+                }
+                else {
+                for (int i = 0; i < Account.globalAccountList.size(); i++) {
+                    if (choice == i) {
+                        displayAccount(Account.globalAccountList.get(i));
+                    }
+                }
+              }
+            }
+            catch (NumberFormatException e) {
+                System.out.println("Invalid number format!");
+                chooseAccount();
             }
         }
-
-
     }
 
 
     public static void createAccount() {
-        System.out.println("Enter Account Type:");
+        System.out.println("Enter Account Type or say 'back' to return to menu:");
         String accountType = scanner.nextLine();
         double balance = 0;
-        Account newAccount  = new Account(accountType, balance);
-        Account.globalAccountList.add(newAccount);
-        mainMenu();
+        if (accountType.equalsIgnoreCase("back")){
+            mainMenu();
+        }
+        else {
+            Account newAccount = new Account(accountType, balance);
+            Account.globalAccountList.add(newAccount);
+            mainMenu();
+        }
     }
+
 
     public static void displayAccount(Account account) {
         account.getAccountType();
@@ -66,11 +87,11 @@ public class Menu {
     public static void userPrompt(Account account) {
         System.out.println("1. Deposit");
         System.out.println("2. Withdraw");
+        System.out.println("3. Back");
         System.out.print("Select Option: ");
         int choice = scanner.nextInt();
         scanner.nextLine();
         System.out.print("\n\n\n\n\n\n\n");
-
         if (choice == 1){
             System.out.println("Enter Deposit Amount:");
             double amountDeposited = scanner.nextInt();
@@ -84,6 +105,9 @@ public class Menu {
             scanner.nextLine();
             account.Withdraw(amountWithdrawn);
             Menu.mainMenu();
+        }
+        else if (choice == 3){
+            chooseAccount();
         }
     }
 
